5faf7467baecef8cc0af440955ac5004d67da48c
jyrs270-dot <jyrs270@gmail.com>
2026-03-22 20:21:45 +0530

    Create duckdb_queries.sql

---
+++ part5-datalake/duckdb_queries.sql
@@ -0,0 +1,18 @@
+-- Q1:
+SELECT c.customer_name, SUM(p.price) AS total_spent
+FROM 'customers.csv' c
+JOIN 'products.parquet' p
+ON c.customer_id = p.customer_id
+GROUP BY c.customer_name
+ORDER BY total_spent DESC
+LIMIT 5;
+
+-- Q2:
+SELECT AVG(order_value) AS avg_order_value
+FROM 'orders.json';
+
+-- Q3:
+CREATE VIEW unified_data AS
+SELECT * FROM 'customers.csv'
+UNION ALL
+SELECT * FROM 'orders.json';
